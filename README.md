0410018104cffa8dbbc347f5399abd48e5ed38d3
liam <ggambuk@gmail.com>
2024-06-01 02:17:33 +0900

    first full-stack

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # datingApp
+서버 스택 = php
+채팅서버 =jsp
---
+++ java/Utills/IPadress.java
@@ -0,0 +1,11 @@
+package Utills;
+
+public class IPadress {
+
+    public Ip ip = new Ip();
+
+    public static class Ip{
+        public String IP = "43.201.38.157";
+        public String IP_Adress = "http://43.201.38.157/";
+    }
+}
